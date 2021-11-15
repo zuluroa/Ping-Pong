43f89fcd00e18eab3581e77f33b0f8807872fb7e
David Zuluaga <zuluroa@gmail.com>
2021-11-15 12:35:22 -0500

    Creación de funciones necesarias (bar,draw) para pintar las barras en el tablero

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-(function(){
-    self.Board = function(width,height){
+(function () {
+    self.Board = function (width, height) {
         this.width = width;
         this.height = height;
         this.playing = false;
@@ -8,28 +8,75 @@
         this.ball = null;
     }
     self.Board.prototype = {
-        get elements(){
+        get elements() {
             var elements = this.bars;
-            elements.push(ball);
+            elements.push(this.ball);
             return elements;
         }
     }
 }());
 
-(function(){
-    self.BoardView = function(canvas,board){
-    this.canvas = canvas;
-    this.canvas.width = board.width;
-    this.canvas.height = board.height;
-    this.board = board;
-    this.context = canvas.getContext("2d");
+(function () {
+    self.Bar = function (x, y, width, height, board) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.board = board;
+
+        this.board.bars.push(this);
+        this.kind = "rectangle";
+    }
+
+    self.Bar.prototype = {
+        down: function () {
+
+        },
+        up: function () {
+
+        }
+    }
+
+}());
+
+
+(function () {
+    self.BoardView = function (canvas, board) {
+        this.canvas = canvas;
+        this.canvas.width = board.width;
+        this.canvas.height = board.height;
+        this.board = board;
+        this.context = canvas.getContext("2d");
+    }
+
+    self.BoardView.prototype = {
+        draw: function () {
+            for (var i = this.board.elements.length - 1; i >= 0; i--) {
+                var el = this.board.elements[i];
+                draw(this.context, el);
+            }
+        }
+    }
+
+    function draw(contexto, element) {
+        if (element != null) {
+            switch (element.kind) {
+                case "rectangle":
+                    contexto.fillRect(element.x, element.y, element.width, element.height);
+                    break;
+            }
+        }
     }
 }());
 
-window.addEventListener("load",main);
+window.addEventListener("load", main);
 
-function main(){
-    var board = new Board(600,300);
+function main() {
+    var board = new Board(800, 400);
+    var bar = new Bar(20,100,40,100,board);
+    var bar = new Bar(700,100,40,100,board);
     var canvas = document.getElementById("canvas");
-    var board_view = new BoardView(canvas,board);
+    var board_view = new BoardView(canvas, board);
+    board_view.draw();
+    console.log(board_view);
 }
